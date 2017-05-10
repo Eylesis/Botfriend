0d84424d9bd211fd87791120f5d53b34473a1a46
Eylesis <kyntenslocum@gmail.com>
2017-05-09 17:44:50 -0700

    Broke Botfriend into Cogs

---
+++ AlchemyLookup.py
@@ -0,0 +1,59 @@
+import discord
+import asyncio
+import json
+import re
+import random
+from discord.ext import commands
+
+class AlchemyLookup():
+    def __init__(self,  bot):
+        self.bot = bot
+        with open('AlchemyTables.json', encoding="utf8") as json_data:
+            self.AlchemyTableMaster = json.load(json_data)
+        with open('Alchemy.json', encoding="utf8") as json_data:
+            self.AlchemyMaterialsMaster = json.load(json_data)
+    
+    @commands.command(pass_context=True)
+    async def material(self, ctx, name: str):
+        author = ctx.message.author
+        print(author.name)
+        i = 0
+        for o in (range(len(self.AlchemyMaterialsMaster))):
+            match = re.search(name, self.AlchemyMaterialsMaster[o]['name'], re.IGNORECASE)
+
+            if match:
+                i = o
+                break
+
+        output = "**{name}**\n*{type}, ({location}) ({dc})*\n{details}\n\n{description}".format(**self.AlchemyMaterialsMaster[i])
+        em = discord.Embed( title="{name}".format(**self.AlchemyMaterialsMaster[i]), description="*{type}, ({location}) ({dc})*\n{details}\n\n{description}".format(**self.AlchemyMaterialsMaster[i]))
+        em.set_footer(text="requested by {user}".format(user=author.name), icon_url=author.avatar_url)
+        await self.bot.say(embed=em)
+        await self.bot.delete_message(ctx.message)
+
+
+    @commands.command(pass_context=True)
+    async def table(self, ctx, tableName: str):
+        author = ctx.message.author
+        print(author.name)
+        i = 0
+        for o in (range(len(self.AlchemyTableMaster))):
+            match = re.search(tableName, self.AlchemyTableMaster[o]['name'], re.IGNORECASE)
+            if match:
+                i = o
+                break
+
+        output = ""
+        for n in range(2,13):
+            if (self.AlchemyTableMaster[i]['subgroup'][n-2] == ""):
+                output += "{0} {1}\n".format(n, self.AlchemyTableMaster[i]['ingredients'][n-2], self.AlchemyTableMaster[i]['subgroup'][n-2])
+            else:
+                output += "{0} {1} (*{2}*)\n".format(n, self.AlchemyTableMaster[i]['ingredients'][n-2], self.AlchemyTableMaster[i]['subgroup'][n-2])
+
+        em = discord.Embed(title="{name} Ingredients".format(**self.AlchemyTableMaster[i]), description=output)
+        em.set_footer(text="requested by {user}".format(user=author.name), icon_url=author.avatar_url)
+        await self.bot.say(embed=em)
+        await self.bot.delete_message(ctx.message)
+
+def setup(bot):
+    bot.add_cog(AlchemyLookup(bot))
--- First.py
+++ First.py
@@ -3,96 +3,57 @@ import asyncio
 import json
 import re
 import random
-from discord.ext.commands import Bot
 from discord.ext import commands
-my_bot = Bot(command_prefix=commands.when_mentioned_or("*"))
 
-with open ('Beastiary.json', encoding="utf8") as json_data:
-    BeastiaryMaster = json.load(json_data)
-print("There are {0} monsters indexed.".format(len(BeastiaryMaster)))
+startup_extensions = ["AlchemyLookup", "BeastiaryLookup"]
 
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("*"))
 
-with open('Alchemy.json', encoding="utf8") as json_data:
-    AlchemyMaterialsMaster = json.load(json_data)
-
-    for i in range(len(AlchemyMaterialsMaster)):    
-        print(("{0}: {1}".format(i+1, AlchemyMaterialsMaster[i]['name'])))
-
-with open('AlchemyTables.json', encoding="utf8") as json_data:
-    AlchemyTableMaster = json.load(json_data)
-
-    for i in range(len(AlchemyTableMaster)):    
-        print(("{0}: {1}".format(i+1, AlchemyTableMaster[i]['name'])))
-
-@my_bot.event
+@bot.event
 async def on_command_error(error, ctx):
     if isinstance(error, discord.ext.commands.CommandNotFound):
         return
 
-@my_bot.event
+@bot.event
 async def on_read():
     print("Client logged in")
 
-@my_bot.command()
+@bot.command()
 async def prefix(newPref: str):
     if newPref != " ":
-        my_bot.command_prefix = commands.when_mentioned_or(newPref)
-        return await my_bot.say("New Prefix `{0}` set!".format(newPref))
+        bot.command_prefix = commands.when_mentioned_or(newPref)
+        return await bot.say("New Prefix `{0}` set!".format(newPref))
     else:
-        return await my_bot.say("New Prefix cannot be `{0}`.".format(newPref))
+        return await bot.say("New Prefix cannot be `{0}`.".format(newPref))
 
-@my_bot.command()
+@bot.command()
 async def hi():
-    return await my_bot.say("Greetings! I am Eramir Melchor!")
+    return await bot.say("Greetings! I am Botfriend!")
 
-@my_bot.event
-async def on_message(message):
-    if message.content.startswith("What are we fighting today, Botfriend?"):
-        randomMonster = random.randrange(0,len(BeastiaryMaster))
-        await my_bot.send_message(message.channel, "Let's fight a {name}.".format(name=BeastiaryMaster[randomMonster]['name']))
-    return await my_bot.process_commands(message)
+
     
+@bot.command()
+async def load(extension_name : str):
+    """Loads an extension."""
+    try:
+        bot.load_extension(extension_name)
+    except (AttributeError, ImportError) as e:
+        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        return
+    await bot.say("{} loaded.".format(extension_name))
 
-@my_bot.command(pass_context=True)
-async def table(ctx, tableName: str):
-    author = ctx.message.author
-    i = 0
-    for o in (range(len(AlchemyTableMaster))):
-        match = re.search(tableName, AlchemyTableMaster[o]['name'], re.IGNORECASE)
+@bot.command()
+async def unload(extension_name : str):
+    """Unloads an extension."""
+    bot.unload_extension(extension_name)
+    await bot.say("{} unloaded.".format(extension_name))
 
-        if match:
-            i = o
-            break
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
 
-    output = ""
-    for n in range(2,13):
-        if (AlchemyTableMaster[i]['subgroup'][n-2] == ""):
-            output += "{0} {1}\n".format(n, AlchemyTableMaster[i]['ingredients'][n-2], AlchemyTableMaster[i]['subgroup'][n-2])
-        else:
-            output += "{0} {1} (*{2}*)\n".format(n, AlchemyTableMaster[i]['ingredients'][n-2], AlchemyTableMaster[i]['subgroup'][n-2])
-
-    em = discord.Embed(title="{name} Ingredients".format(**AlchemyTableMaster[i]), description=output)
-    em.set_footer(text="requested by {user}".format(user=author.name), icon_url=author.avatar_url)
-    await my_bot.say(embed=em)
-    return await my_bot.delete_message(ctx.message)
-    
-
-@my_bot.command(pass_context=True)
-async def material(ctx, name: str):
-    author = ctx.message.author
-    i = 0
-    for o in range(len(AlchemyMaterialsMaster)):
-        match = re.search(name, AlchemyMaterialsMaster[o]['name'], re.IGNORECASE)
-
-        if match:
-            i = o
-            
-            break
-
-    output = "**{name}**\n*{type}, ({location}) ({dc})*\n{details}\n\n{description}".format(**AlchemyMaterialsMaster[i])
-    em = discord.Embed( title="{name}".format(**AlchemyMaterialsMaster[i]), description="*{type}, ({location}) ({dc})*\n{details}\n\n{description}".format(**AlchemyMaterialsMaster[i]))
-    em.set_footer(text="requested by {user}".format(user=author.name), icon_url=author.avatar_url)
-    await my_bot.say(embed=em)
-    return await my_bot.delete_message(ctx.message)
-
-my_bot.run("MjQzNDk0NzkwMzU5MDIzNjE2.CvwNvQ.lCk5xfkWCtFzle8IaqLz2ki6Et0")
+    bot.run("MzA5MDE0MDY5MTMyNjU2NjQx.C_OiRw.v2nP6a82r2VNJRTWpU2PBWjgzWI")
