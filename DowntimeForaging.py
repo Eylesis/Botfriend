fe84f565e1c99589da723b182525fe5260ec065c
Eylesis <kyntenslocum@gmail.com>
2017-06-08 21:04:14 -0700

    Downtime Foraging Menu

--- DowntimeForaging.py
+++ DowntimeForaging.py
@@ -5,6 +5,7 @@ import json
 import re
 import dice
 import shlex
+import math
 from functions import parse_args_3
 from functions import list_get
 from discord.ext import commands
@@ -15,6 +16,9 @@ class DowntimeForaging():
         with open('AlchemyTables.json', encoding="utf8") as json_data:
             self.AlchemyTableMaster = json.load(json_data)
     
+    def roundup(self, x):
+        return int(math.ceil(x / 10.0)) * 10
+
     def Get_Table(self, name):
         i = 0
         usingTable = []
@@ -67,6 +71,8 @@ class DowntimeForaging():
                 break
         if (foundCommon):
             tableRoll = dice.roll('2d6rr7').total
+            while tableRoll == 7:
+                tableRoll = dice.roll('2d6rr7').total
             selectedMat = self.Get_Table('common')[tableRoll-2]
             args['quantities'].append(entryQuantity)
             args['materials'].append(selectedMat)
@@ -103,18 +109,18 @@ class DowntimeForaging():
         args['logOutput'] = logOutput[3:-3]
         embed=discord.Embed(title="Log Output", description=logOutput)
         return embed
-    def Construct_Output(self, args, author):
+    
+    def Construct_Output(self, args, author, startRange, endRange):
+        
         outQuantity = ''
         outMaterial = ''
         outRoll = ''
-        for entries in args['quantities']:
+        for entries in args['quantities'][startRange:endRange]:
             outQuantity += "{0}\n".format(entries)
-        for entries in args['materials']:
+        for entries in args['materials'][startRange:endRange]:
             outMaterial += "{0}\n".format(entries)
-        for entries in args['herbalismRolls']:
+        for entries in args['herbalismRolls'][startRange:endRange]:
             outRoll += "{0}\n".format(entries)
-
-
 
         embed=discord.Embed(title="Foraging Results", description="{character} has foraged in the {biome}! They managed to find the following:".format(**args))
         embed.add_field(name="Quantity", value=outQuantity, inline=True)
@@ -124,7 +130,42 @@ class DowntimeForaging():
         embed.set_footer(text="requested by {user}".format(user=author.name), icon_url=author.avatar_url)
         return embed
 
-    
+    async def reaction_menu(self, args, author, message, logMessage):
+        toggle = False
+        page = 0
+        pgEnd = 0
+        maxPage = self.roundup(len(args['quantities'])) / 10
+        print(maxPage)
+        await self.bot.add_reaction(message, '\U000025c0')
+        await self.bot.add_reaction(message, '\U000025b6')
+        await self.bot.add_reaction(message, '\U0001f4cb')
+        while toggle==False:
+            res = await self.bot.wait_for_reaction(message=message, user=author)
+            print('start: {0}'.format(page))
+            if res.reaction.emoji == '\U0001f4cb':
+                print ('clipboard')
+                logChannel = self.bot.get_channel('243574826290118666')
+                await self.bot.send_message(logChannel, args['logOutput'])
+                await self.bot.clear_reactions(message)
+                toggle = True
+            elif res.reaction.emoji == '\U000025c0' and page > 0:
+                print ('leftArrow')
+                page -= 1
+                pgEnd = int(page*10)+10
+                logMessage = await self.bot.edit_message(logMessage, embed=self.Construct_Output(args, author, (page*10), pgEnd))
+                await self.bot.remove_reaction(message, '\U000025c0', author)
+            elif res.reaction.emoji == '\U000025b6' and page <= maxPage:
+                print ('rightArrow')
+                page += 1
+                pgEnd = int(page*10)+10
+                logMessage = await self.bot.edit_message(logMessage, embed=self.Construct_Output(args, author, (page*10), pgEnd))
+                await self.bot.remove_reaction(message, '\U000025b6', author)
+            else:
+                print ('nope')
+            print('end: {0}'.format(page))
+        print ('exited')
+        return
+
     @commands.command(pass_context=True)
     async def forage(self, ctx, biome: str, rollstring: str, *, args=None):
         author = ctx.message.author
@@ -165,18 +206,9 @@ class DowntimeForaging():
         if not argArray['quantities']:
             await self.bot.say(embed=self.Construct_Failure(argArray, author))
         else:
-            print (collectionOutput)
-            await self.bot.say(embed=self.Construct_Output(argArray, author))
+            logMsg = await self.bot.say(embed=self.Construct_Output(argArray, author, 0, 10))
             msg = await self.bot.send_message(ctx.message.channel, embed=self.Construct_Log(argArray, collectionOutput, author))
-            await self.bot.add_reaction(msg, '\U0001f4cb')
-            res = await self.bot.wait_for_reaction('\U0001f4cb', message=msg, user=author)
-            logChannel = self.bot.get_channel('243574826290118666')
-            await self.bot.send_message(logChannel, argArray['logOutput'])
-            await self.bot.clear_reactions(msg)
-
-        
-
-   
+            await self.reaction_menu(argArray, author, msg, logMsg)
 
 def setup(bot):
     bot.add_cog(DowntimeForaging(bot))
