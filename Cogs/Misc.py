a64eee28001eb332614eafe50c2c7fd4fa0a1400
Eylesis <kyntenslocum@gmail.com>
2018-06-05 15:56:43 -0500

    typo command

--- Cogs/Misc.py
+++ Cogs/Misc.py
@@ -6,7 +6,7 @@ class Misc():
         self.bot = bot
     
     @commands.command(pass_context=True)
-    async def bana(self, ctx):
+    async def banana(self, ctx):
         await bot.say('Why thank you {}! I do so enjoy bananas, however I am not hungry at the moment, so I shall save this for later!'.format(ctx.message.author.mention))
 
 def setup(bot):
