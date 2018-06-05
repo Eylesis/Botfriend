63dfb6136a3f57db0b9cb58b85005377c06b121c
Eylesis <kyntenslocum@gmail.com>
2018-06-05 13:05:04 -0500

    Added Dicecloud vs Others stats comparisons

--- Cogs/CharacterComparator.py
+++ Cogs/CharacterComparator.py
@@ -32,12 +32,17 @@ class Comparator():
             HP = {'lowName': rando['stat_cvars']['name'], 'lowHP' : rando['stat_cvars']['hp'], 'highName': rando['stat_cvars']['name'], 'highHP' : rando['stat_cvars']['hp']}
             lowStats = {'strName' : rando['stat_cvars']['name'], 'strScore' : rando['stat_cvars']['strength'], 'dexName' : rando['stat_cvars']['name'], 'dexScore' : rando['stat_cvars']['dexterity'], 'conName': rando['stat_cvars']['name'], 'conScore'  : rando['stat_cvars']['constitution'], 'intName': rando['stat_cvars']['name'], 'intScore'  : rando['stat_cvars']['intelligence'], 'wisName': rando['stat_cvars']['name'], 'wisScore'  : rando['stat_cvars']['wisdom'], 'chaName': rando['stat_cvars']['name'], 'chaScore'  : rando['stat_cvars']['charisma']}
             break
+        
         totalChars = 0
+        diceclouds = 0
+        others = 0
 
         for users, characters in respData.items():
             totalChars += 1
-            print(str(characters['stat_cvars']['name']) + " : " + str(characters['armor'])) 
-            print(str(characters['stat_cvars']['strength']) + "," + str(characters['stat_cvars']['dexterity']) + "," + str(characters['stat_cvars']['constitution']) + "," + str(characters['stat_cvars']['intelligence']) + "," + str(characters['stat_cvars']['wisdom']) + "," + str(characters['stat_cvars']['charisma']))
+            if characters['type'] == 'dicecloud':
+                diceclouds += 1
+            else:
+                others += 1
             if characters['armor'] < AC['lowAC']:
                 AC['lowAC'] = characters['armor']
                 AC['lowName'] = characters['stat_cvars']['name']
@@ -72,7 +77,7 @@ class Comparator():
         embed = discord.Embed(title="Planar Marches Interesting Character Stats!", description="Here are a few interesting statistics that I can pull up from Avrae's Database for Planar Marches! If you have any other interesting statistics you'd like to see, feel free to ask!")
 
         embed.set_footer(text="Requested by: {}".format(ctx.message.author.display_name), icon_url=ctx.message.author.avatar_url)
-        embed.add_field(name="Total Registered Characters", value=totalChars)
+        embed.add_field(name="Total Registered Characters", value="{} (Dicecloud: {} | Other: {})".format(totalChars, diceclouds, others))
         embed.add_field(name="Lowest | Highest", value="Base AC: {0[lowName]}: {0[lowAC]} | {0[highName]}: {0[highAC]} \nHP: {1[lowName]}: {1[lowHP]} | {1[highName]}: {1[highHP]}".format(AC, HP))
         embed.add_field(name="Lowest Ability Scores", value="Strength: {0[strName]}: {0[strScore]} \nDexterity: {0[dexName]}: {0[dexScore]} \nConstitution: {0[conName]}: {0[conScore]} \nIntelligence: {0[intName]}: {0[intScore]} \nWisdom: {0[wisName]}: {0[wisScore]} \nCharisma: {0[chaName]}: {0[chaScore]}".format(lowStats))
 
