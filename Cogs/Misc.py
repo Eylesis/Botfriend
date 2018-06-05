dcbfb90bb9a0b1ef3cd9ce8b886e1b7308843ead
Eylesis <kyntenslocum@gmail.com>
2018-06-05 15:58:20 -0500

    self in python is dumb

--- Cogs/Misc.py
+++ Cogs/Misc.py
@@ -7,7 +7,7 @@ class Misc():
     
     @commands.command(pass_context=True)
     async def banana(self, ctx):
-        await bot.say('Why thank you {}! I do so enjoy bananas, however I am not hungry at the moment, so I shall save this for later!'.format(ctx.message.author.mention))
+        await self.bot.say('Why thank you {}! I do so enjoy bananas, however I am not hungry at the moment, so I shall save this for later!'.format(ctx.message.author.mention))
 
 def setup(bot):
     bot.add_cog(Misc(bot))
