b87e439f584e2ed4ec58f889d5bb23823f190419
Eylesis <kyntenslocum@gmail.com>
2018-06-09 02:17:07 -0500

    Wrapped role into an object before converting to a string

--- Cogs/PingAlerts.py
+++ Cogs/PingAlerts.py
@@ -9,7 +9,10 @@ class PingAlerts():
     async def PingAlerts(self, ctx, minlevel : int, maxlevel : int):
         output = ''
         for x in range(minlevel, maxlevel+1):
-            output += '<@lvl {}> '.format(x)           
+            new_name = "Lvl {}".format(level)
+            new_role = discord.utils.get(message.server.roles, name=new_name)
+            mentionString = new_role.mention
+            output += '{} '.format(mentionString)           
         await self.bot.say('{} has requested a notification be sent out to all players possessing the following roles for a posted quest!: {}'
         .format(ctx.message.author.mention, output))
 
