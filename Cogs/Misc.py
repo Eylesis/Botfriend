96bd6eb954fc1fc65b9c5ab2f3f8ae189c6bfaf2
Eylesis <kyntenslocum@gmail.com>
2018-06-28 16:39:43 -0500

    Deletes triggering message for banana command

--- Cogs/Misc.py
+++ Cogs/Misc.py
@@ -7,6 +7,7 @@ class Misc():
     
     @commands.command(pass_context=True)
     async def banana(self, ctx):
+        await self.bot.delete_message(ctx.message)
         await self.bot.say('Why thank you {}! I do so enjoy bananas, however I am not hungry at the moment, so I shall save this for later!'.format(ctx.message.author.mention))
 
     @commands.command(pass_context=True, hidden=True)
