255c049e79b353d17870f9d2d18b7ba793cee82a
Eylesis <kyntenslocum@gmail.com>
2018-07-04 16:38:23 -0500

    added persistent stash count to banana command

--- Cogs/Misc.py
+++ Cogs/Misc.py
@@ -1,4 +1,5 @@
 import discord
+import redisInterface
 from discord.ext import commands
 
 class Misc():
@@ -7,7 +8,15 @@ class Misc():
     
     @commands.command(pass_context=True)
     async def banana(self, ctx):
-        await self.bot.say('Why thank you {}! I do so enjoy bananas, however I am not hungry at the moment, so I shall save this for later!'.format(ctx.message.author.mention))
+        bananaStash = self.bot.db.get_val('bananaStash')
+        if bananaStash = '':
+            await self.bot.say('Why thank you {}! This banana is the first in my latest stash! I shall store it carefully.'.format(ctx.message.author.mention))
+            self.bot.db.set_val('bananaStash', 1)
+        else:
+            bananaStash += 1
+            self.bot.db.set_val('bananaStash', bananaStash)
+            await self.bot.say('Why thank you {}! I do so enjoy bananas, however I am not hungry at the moment, so I shall save this for later! My stash has {} bananas in it, can you believe it?'
+            .format(ctx.message.author.mention, bananaStash))
 
     @commands.command(pass_context=True, hidden=True)
     async def chanSay(self, ctx, channel: str, *, message: str):
