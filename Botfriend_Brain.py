7ab590a4fbfbaa789c26c414d3350edd7350a1a3
Eylesis <kyntenslocum@gmail.com>
2018-07-11 14:02:23 -0500

    added weather and campaigntools cogs to load

--- Botfriend_Brain.py
+++ Botfriend_Brain.py
@@ -11,7 +11,7 @@ botToken = os.environ.get('botToken')
 
 description = '''Botfriend Configuration: Conversational ^-^'''
 
-startup_extensions = ["Cogs.help", "Cogs.autorole", "Cogs.PingAlerts", "Cogs.Misc"]
+startup_extensions = ["Cogs.help", "Cogs.autorole", "Cogs.PingAlerts", "Cogs.Misc", "Cogs.Weather", "Cogs.CampaignTools"]
 # "Cogs.GameAlerts", "Cogs.CharacterComparator", "Cogs.Misc"
 bot = commands.Bot(command_prefix='*', description=description)
 bot.remove_command('help')
