f11d10a6f1cc249e35b44b721b9086167cc1f45c
Eylesis <kyntenslocum@gmail.com>
2018-07-22 01:41:45 -0500

    Readded literals?

--- KofiFriend_Brain.py
+++ KofiFriend_Brain.py
@@ -40,7 +40,7 @@ async def tba_handler(request):
         title="Crooq's Computer Quest Updated!", 
         url="https://ko-fi.com/eylesis", 
         description="{} has given ${} to the cause! The donation is appreciated!".format(data['from_name'], data['amount']),
-        timestamp=datetime.datetime.strptime(data['timestamp'], "%Y-%m-%d %I:%M:%S.%f"))
+        timestamp=datetime.datetime.strptime(data['timestamp'], "%Y-%m-%dT%I:%M:%S.%fZ"))
     embed.set_footer(text="Ko-Fi")
     embed.add_field(name="__Message__", value=data['message'])
 
