ae6e6264eddd21d869133291b297186368c50954
Eylesis <kyntenslocum@gmail.com>
2018-07-30 18:29:26 -0500

    Added a help command and admin locked ungold command.

--- Cogs/GoogleManip.py
+++ Cogs/GoogleManip.py
@@ -15,32 +15,43 @@ class GoogleManip():
         self.gc = pygsheets.authorize(service_file='Botfriend-creds.json')
     
     @commands.command(pass_context=True)
-    async def ungold(self, ctx):
-        userGoldData = self.bot.db.get_val('UserGoldData')
-        userGoldData.pop(ctx.message.author.id, None)
-        self.bot.db.set_val('UserGoldData', userGoldData)
-        await self.bot.say('done.')
+    async def ungold(self, ctx, userID):
+        if ctx.messaage.author.id == '227168575469780992':
+            userGoldData = self.bot.db.get_val('UserGoldData')
+            userGoldData.pop(userID, None)
+            self.bot.db.set_val('UserGoldData', userGoldData)
+            return await self.bot.say('I have removed this ID from the Gold Tracking Project.')
+        else:
+            return await self.bot.say('Terribly sorry {0.author.mention}, but I do not recognize you as a person of authority here!'.format(ctx.message))
 
     @commands.command(pass_context=True)
-    async def gold(self, ctx, sheetUrl : str):
-        
-        userGoldData = self.bot.db.get_val('UserGoldData', {})
-
-        if not ctx.message.author.id in userGoldData:
+    async def gold(self, ctx, sheetUrl=None):
+        if sheetUrl == None:
+            embed = discord.Embed(title="Gold Tracking Project!", 
+            description="So, you want to be apart of the gold tracking project? Well good on you! This project is an attempt to collect tangible data on the recent gold changes for the Planar Marches Moderation Team to analyze. So, how do you join the initiative? It's simple, allow me to walk you through the process!")
             
-            try:
-               sh = self.gc.open_by_url(sheetUrl)
-            except:
-                return await self.bot.say('I have run into an error attempting to access this sheet. Have you shared it to me?')
-            wks = sh.sheet1
-            charName = wks.cell('L8')
-            startBal = wks.cell('L9')
-
-            userGoldData[ctx.message.author.id] = {'CharName' : charName.value, "StartBal" : startBal.value, "sheetUrl" : sheetUrl}
-            self.bot.db.set_val('UserGoldData', userGoldData)
-            return await self.bot.say('{}, it is really appreciated that you have opted to join this data collection initiative! I have gone ahead and added {} to our listings!'.format(ctx.message.author.mention, charName.value))
+            embed.add_field(name="Register a Sheet", value="First, grab a copy of this [google sheet](https://docs.google.com/spreadsheets/d/1n5Mpnvrw5T-lfDtGRNnP8ePwdR9WPcJf-XAuu42Thtw/edit?usp=sharing). It is your sheet to log any and all gold income and expense. It behaves like a standard balancing sheet. If you have questions, be sure to visit the help-zone channel! To register the sheet, share the sheet with me, Botfriend! \nMy google email is `botfriend@botfriend-3393.iam.gserviceaccount.com`, just give me edit permissions to your excel sheet. Once that has been completed, all that's left is to run this command again, with your sheet url! `*gold url`")
+            embed.add_field(name="How to Setup a Sheet", value="The sheet looks complex, right? Well it really is quite a complex task of handling gold. Luckily for you, there are only a few requirements to set up the sheet. You will notice two fields in the top right of the sheet. **Character Name**, and **Starting Balance**. The **Character Name** must match exactly the name of your character as seen on your Avrae sheet. We use this to make sure you're not accidentally using the level of a different character! And the **Starting Balance** is whatever gold total you have on your sheet when you first set up the sheet. These two values are static. Once you set them, don't touch them! The other section you can customize is the Income and Expense categories. You can change the names on the first page of these categories to suit your characters income types. Not a caster and don't need Spell Components? Replace it with a fancy dinners expense category!")
+            embed.add_field(name="Sending your Data to Botfriend.", value="Once you have received a confirmation message from me following the **Register a Sheet** step, you're actually all set! Everytime you run the `!update` command with your registered character, I will access your transaction sheet and pull the relevant data. You don't have to do anything new!")
+            return await self.bot.say(embed=embed)
         else:
-            return await self.bot.say("{}, it would appear I already have one of your characters registered! Submitting your data is now automated. Simply keep your Gold Tracking Sheet up to date before running Avrae's `!update` command!".format(ctx.message.author.mention))
+            userGoldData = self.bot.db.get_val('UserGoldData', {})
+
+            if not ctx.message.author.id in userGoldData:
+                
+                try:
+                    sh = self.gc.open_by_url(sheetUrl)
+                except:
+                    return await self.bot.say('I have run into an error attempting to access this sheet. Have you shared it to me?')
+                wks = sh.sheet1
+                charName = wks.cell('L8')
+                startBal = wks.cell('L9')
+
+                userGoldData[ctx.message.author.id] = {'CharName' : charName.value, "StartBal" : startBal.value, "sheetUrl" : sheetUrl}
+                self.bot.db.set_val('UserGoldData', userGoldData)
+                return await self.bot.say('{}, it is really appreciated that you have opted to join this data collection initiative! I have gone ahead and added {} to our listings!'.format(ctx.message.author.mention, charName.value))
+            else:
+                return await self.bot.say("{}, it would appear I already have one of your characters registered! Submitting your data is now automated. Simply keep your Gold Tracking Sheet up to date before running Avrae's `!update` command!".format(ctx.message.author.mention))
 
     async def on_message(self, message):
         if message.content.startswith('!update'):
