018a1b058617118d98d1475b062f72ae061b9f5f
Eylesis <kyntenslocum@gmail.com>
2018-08-20 01:40:14 -0500

    Disabled Gold Collection

--- Botfriend_Brain.py
+++ Botfriend_Brain.py
@@ -13,7 +13,7 @@ botToken = os.environ.get('botToken')
 description = '''Botfriend Configuration: Conversational ^-^'''
 
 #startup_extensions = []
-startup_extensions = ["Cogs.help", "Cogs.autorole", "Cogs.PingAlerts", "Cogs.Misc", "Cogs.Weather", "Cogs.CampaignTools", "Cogs.GoogleManip", "Cogs.DataBaseTools", "Cogs.RulesLookup", "Cogs.DmGold"]
+startup_extensions = ["Cogs.help", "Cogs.autorole", "Cogs.PingAlerts", "Cogs.Misc", "Cogs.Weather", "Cogs.CampaignTools", "Cogs.DataBaseTools", "Cogs.RulesLookup", "Cogs.DmGold"]
 # "Cogs.GameAlerts", "Cogs.CharacterComparator", "Cogs.Misc"
 bot = commands.Bot(command_prefix='*', description=description)
 bot.remove_command('help')
