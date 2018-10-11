f6f0223e44fd51316572d4e6bb5115fb298df740
Eylesis <kyntenslocum@gmail.com>
2018-10-11 17:53:49 -0500

    fix send?

--- Cogs/Misc.py
+++ Cogs/Misc.py
@@ -53,7 +53,7 @@ class Misc():
                 out_im.paste(image, (x_offset,0))
                 x_offset += image.size[0]
             out_im.save('images/out.png')
-            self.bot.send_file(message.channel, 'images/out.png')
+            self.bot.send_file(message.channel, r'images/out.png', filename="cat.png")
 
 def setup(bot):
     bot.add_cog(Misc(bot))
