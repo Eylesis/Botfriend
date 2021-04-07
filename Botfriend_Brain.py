9a5058e0e8c542595dcfcd48b8b4b28613eeb358
Eylesis <kyntenslocum@gmail.com>
2021-04-07 15:21:15 -0500

    adding weather

--- Botfriend_Brain.py
+++ Botfriend_Brain.py
@@ -12,7 +12,7 @@ botToken = os.environ.get('botToken')
 description = '''Botfriend Configuration: Conversational ^-^'''
 
 #startup_extensions = []
-startup_extensions = ["Cogs.Misc", "Cogs.ServerCopy"]
+startup_extensions = ["Cogs.Misc", "Cogs.Weather"]
 # "Cogs.GameAlerts", "Cogs.CharacterComparator", "Cogs.Misc"
 bot = commands.Bot(command_prefix='*', description=description)
 bot.remove_command('help')
